43012163d236539af37983898044e3c6e800dfb5
SarthakJShetty <sarthakshetty97@gmail.com>
2018-09-12 21:34:04 +0530

    Some code has been moved to common_functions.py

--- Scraper.py
+++ Scraper.py
@@ -20,96 +20,9 @@ from urllib.request import urlopen
 import urllib.error
 '''BeautifulSoup is used for souping.'''
 from bs4 import BeautifulSoup
-'''datetime is used while building the database logs'''
-from datetime import datetime
-'''Used to create the database log'''
-import os
-'''argparse has been imported to extract keyword(s)'''
-import argparse
-
-def arguments_parser():
-	'''This function is used to read the initial keyword that will be queried in ScienceDirect (for now).
-	We will be scrapping Science, Nature etc later, as long as generic URLs are supported.'''
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--keywords", help="Keyword to search on ScienceDirect", default="Tiger")
-	arguments = parser.parse_args()
-	if arguments.keywords:
-		keywords = arguments.keywords
-	keywords = keywords.split()
-	return keywords
-
-def pre_processing(keywords):
-	'''This function contains all the pre-processing statements related to the running of the program, including:
-	1. Default starter URL of the page to scrape.
-	2. Default location of LOGS, including:
-		1. Location of Abstract_ID_Database
-		2. Location of Abstract_Database'''
-
-	'''Declaring the time and date variables here. Year, month, day, hours, minute & seconds.'''
-	run_start_year = str(datetime.now().date().year)
-	run_start_month = str(datetime.now().date().month)
-	run_start_day = str(datetime.now().date().day)
-	run_start_date = str(datetime.now().date())
-	run_start_hour = str(datetime.now().time().hour)
-	run_start_minute = str(datetime.now().time().minute)
-	run_start_second = str(datetime.now().time().second)
-	'''Keywords have to be written into the filename of the LOG that we are running'''
-	folder_attachement = ""
-	if(len(keywords)==1):
-		folder_attachement = keywords[0]
-	else:
-		for keyword_index in range(0, len(keywords)):
-			if((keyword_index+1)==len(keywords)):
-				folder_attachement = folder_attachement+keywords[keyword_index]
-			else:
-				folder_attachement = folder_attachement+keywords[keyword_index]+"_"
-	'''Declaring the LOG folder and the abstract, abstract_id & status_logger files.'''
-	logs_folder_name = "LOGS"+"/"+"LOG"+"_"+run_start_date+'_'+run_start_hour+'_'+run_start_minute+"_"+folder_attachement
-	abstract_id_log_name =logs_folder_name+"/"+'Abstract_ID_Database'+'_'+run_start_date+'_'+run_start_hour+'_'+run_start_minute+"_"
-	abstracts_log_name = logs_folder_name+"/"+'Abstract_Database'+'_'+run_start_date+'_'+run_start_hour+'_'+run_start_minute
-	status_logger_name = logs_folder_name+"/"+'Status_Logger'+'_'+run_start_date+'_'+run_start_hour+'_'+run_start_minute
-
-	'''If the filename does not exist create the file in the LOG directory'''
-	if not os.path.exists(logs_folder_name):
-		os.makedirs(logs_folder_name)
-	
-	'''Creating the status_log and writing the session duration & date'''
-	status_log = open(status_logger_name+'.txt', 'a')
-	status_log.write("Session:"+" "+run_start_day+"/"+run_start_month+"/"+run_start_year+"\n")
-	status_log.write("Time:"+" "+run_start_hour+":"+run_start_minute+":"+run_start_second+"\n")
-	status_log.close()
-
-	logs_folder_name_status_key = "Built LOG folder for session"
-	status_logger(status_logger_name, logs_folder_name_status_key)
-
-	query_string = ""
-	if (len(keywords)==1):
-		query_string = keywords[0]
-	else:
-		for keyword_index in range(0, len(keywords)):
-			if((keyword_index+1)==len(keywords)):
-				query_string = query_string+keywords[keyword_index]
-			else:
-				query_string = query_string+keywords[keyword_index]+"%20"
-	start_url = "https://www.sciencedirect.com/search?qs="+query_string+"&show=100&sortBy=relevance&accessTypes=openaccess%2Copenarchive&offset="
-	urls_to_scrape = url_generator(start_url, status_logger_name)
-	abstract_url = 'https://www.sciencedirect.com/science/article/pii/'
-
-	return abstract_url, urls_to_scrape, abstract_id_log_name, abstracts_log_name, status_logger_name
-
-def status_logger(status_logger_name, status_key):
-	'''Status logger to print and log details throught the running the program.
-	Declaring current_hour, current_minute & current_second.'''
-	current_hour = str(datetime.now().time().hour)
-	current_minute = str(datetime.now().time().minute)
-	current_second = str(datetime.now().time().second)
-
-	'''Logging the complete_status_key and printing the complete_status_key'''
-	complete_status_key = "[INFO]"+current_hour+":"+current_minute+":"+current_second+" "+status_key
-	print(complete_status_key)
-	status_log = open(status_logger_name+'.txt', 'a')
-	status_log.write(complete_status_key+"\n")
-	status_log.close()
+'''Fragmenting code into different scripts. Some functions are to be used across the different sub-parts as well.
+Hence, shifted some of the functions to the new script.'''
+from pre_processing_functions import pre_processing, arguments_parser,  status_logger
 
 def url_reader(url, status_logger_name):
 	'''This keyword is supplied to the URL and is hence used for souping.''' 
@@ -254,12 +167,13 @@ def title_scraper(abstract_soup):
 	return title
 
 def end_process(status_logger_name):
+	'''Self-explanatory, this function declares successful completion of the code.'''
 	end_process_status_key="Process has successfully ended"
 	status_logger(status_logger_name, end_process_status_key)
 
 def abstract_id_scraper(abstract_id_log_name, page_soup, site_url_index, status_logger_name):
 	'''This function helps in obtaining the PII number of the abstract.
-	This number is then coupled with the dynamic url and provides'''
+	This number is then coupled with the dynamic URL and provides'''
 
 	abstract_id_scraper_start_status_key="Scraping IDs"
 	status_logger(status_logger_name, abstract_id_scraper_start_status_key)
@@ -284,16 +198,14 @@ def processor(abstract_url, urls_to_scrape, abstract_id_log_name, abstracts_log_
 		'''Actually obtaining the abstracts after combining ID with the abstract_url'''
 		abstract_crawler(abstract_url, abstract_id_log_name, abstracts_log_name, site_url_index, status_logger_name)
 
-def scraper_main():
+def scraper_main(keywords_to_search):
 	''''This function contains all the functions and contains this entire script here, so that it can be imported later to the main function'''
 
-	'''This function collects keywords'''
-	keywords_to_search = arguments_parser()
 	'''Keyword is passed on to pre_processing() declares the url of the site and the abstract collection url'''
-	abstract_url, urls_to_scrape, abstract_id_log_name, abstracts_log_name, status_logger_name = pre_processing(keywords_to_search)
+	abstract_id_log_name, abstracts_log_name, start_url, abstract_url, status_logger_name = pre_processing(keywords_to_search)
+	'''Provides the links for the URLs to be scraped by the scraper'''
+	urls_to_scrape = url_generator(start_url, status_logger_name)
 	'''Calling the processor() function here'''
 	processor(abstract_url, urls_to_scrape, abstract_id_log_name, abstracts_log_name, status_logger_name, keywords_to_search)
 	'''End process function'''
 	end_process(status_logger_name)
-
-scraper_main()
