399119a66501a5b2894a1cfa7752083d05d896c4
SarthakJShetty <sarthakshetty97@gmail.com>
2018-11-27 15:46:14 +0530

    Updated scraper to Springer

--- Scraper.py
+++ Scraper.py
@@ -32,27 +32,27 @@ def url_reader(url, status_logger_name):
 	except (UnboundLocalError, urllib.error.HTTPError):
 		pass
 
-def url_generator(start_url, status_logger_name):
+def url_generator(start_url, query_string, status_logger_name):
 	'''This function is written to scrape all possible webpages of a given topic
 	The search for the URLs truncates when determiner doesn't return a positive value'''
 	url_generator_start_status_key = start_url+" "+"start_url has been received"
 	status_logger(status_logger_name, url_generator_start_status_key)
 	urls_to_scrape=[]
 	counter = 0
-	total_url = start_url+str(counter)
+	total_url = start_url+str(counter)+"?query="+query_string
 	initial_url_status_key = total_url+" "+"has been obtained"
 	status_logger(status_logger_name, initial_url_status_key)
 	urls_to_scrape.append(total_url)
 	test_soup = BeautifulSoup(urlopen(total_url), 'html.parser')
-	determiner = test_soup.find('ol', {'class':'search-result-wrapper'})
+	determiner = test_soup.find('a', {'class':'title'})
 	'''This while loop continuously pings and checks for new webpages, then stores them for scraping'''
 	while(determiner):
-		counter = counter+100
-		total_url = start_url+str(counter)
+		counter = counter+1
+		total_url = start_url+str(counter)+"?query="+query_string
 		url_generator_while_status_key=total_url+" "+"has been obtained"
 		status_logger(status_logger_name, url_generator_while_status_key)
 		soup = BeautifulSoup(urlopen(total_url), 'html.parser')
-		determiner = soup.find('ol', {'class':'search-result-wrapper'})
+		determiner = soup.find('a', {'class':'title'})
 		urls_to_scrape.append(total_url)
 	urls_to_scrape.pop(len(urls_to_scrape)-1)
 	#print(urls_to_scrape)
@@ -142,7 +142,7 @@ def abstract_crawler(abstract_url, abstract_id_log_name, abstracts_log_name, sit
 	abstract_input_tag_ids = abstract_id_database_reader(abstract_id_log_name, abstract_crawler_temp_index, status_logger_name)
 	for abstract_input_tag_id in abstract_input_tag_ids:
 		try:
-			abstract_crawler_accept_status_key="Abstract Number:"+" "+str((abstract_input_tag_ids.index(abstract_input_tag_id)+1)+abstract_crawler_temp_index*100)
+			abstract_crawler_accept_status_key="Abstract Number:"+" "+str((abstract_input_tag_ids.index(abstract_input_tag_id)+1)+abstract_crawler_temp_index*20)
 			status_logger(status_logger_name, abstract_crawler_accept_status_key)
 			abstract_page_scraper(abstract_url, abstract_input_tag_id, abstracts_log_name, status_logger_name)
 		except TypeError:
@@ -152,17 +152,20 @@ def abstract_crawler(abstract_url, abstract_id_log_name, abstracts_log_name, sit
 
 def abstract_scraper(abstract_soup):
 	'''This function scrapes the abstract from the soup and returns to the page scraper'''
-	abstract = str(abstract_soup.find('div', {'class':'Abstracts'}).text.encode('utf-8'))[1:]
+	abstract = str(abstract_soup.find('p', {'class':'Para'}).text.encode('utf-8'))[1:]
 	return abstract
 
 def author_scraper(abstract_soup):
 	'''This function scrapes the author of the text, for easy navigation and search'''
-	author = str(abstract_soup.find('span', {'class':'text given-name'}).text.encode('utf-8'))[1:]
+	author = str(abstract_soup.find('span', {'class':'authors__name'}).text.encode('utf-8'))[1:]
 	return author
 
 def title_scraper(abstract_soup):
 	'''This function scrapes the title of the text'''
-	title = str(abstract_soup.find('h1',{'class':'Head'}).text.encode('utf-8'))[1:]
+	try:
+		title = str(abstract_soup.find('h1',{'class':'ArticleTitle'}).text.encode('utf-8'))[1:]
+	except AttributeError:
+		title = str(abstract_soup.find('h1',{'class':'ChapterTitle'}).text.encode('utf-8'))[1:]
 	return title
 
 def abstract_id_scraper(abstract_id_log_name, page_soup, site_url_index, status_logger_name):
@@ -172,9 +175,9 @@ def abstract_id_scraper(abstract_id_log_name, page_soup, site_url_index, status_
 	abstract_id_scraper_start_status_key="Scraping IDs"
 	status_logger(status_logger_name, abstract_id_scraper_start_status_key)
 	''''This statement collects all the input tags that have the abstract ids in them'''
-	abstract_input_tags = page_soup.findAll('a', {'class':'purchase-link'})
+	abstract_input_tags = page_soup.findAll('a', {'class':'title'})
 	for abstract_input_tag in abstract_input_tags:
-		abstract_input_tag_id=abstract_input_tag.get('aria-describedby')
+		abstract_input_tag_id=abstract_input_tag.get('href')
 		abstract_id_database_writer(abstract_id_log_name, abstract_input_tag_id, site_url_index)
 
 	abstract_id_scraper_stop_status_key="Scraped IDs"
@@ -182,6 +185,7 @@ def abstract_id_scraper(abstract_id_log_name, page_soup, site_url_index, status_
 
 def processor(abstract_url, urls_to_scrape, abstract_id_log_name, abstracts_log_name, status_logger_name, keywords_to_search):
 	''''Multiple page-cycling function to scrape multiple result pages'''
+	print(len(urls_to_scrape))
 	for site_url_index in range(0, len(urls_to_scrape)):
 		'''Collects the web-page from the url for souping'''
 		page_to_soup = url_reader(urls_to_scrape[site_url_index], status_logger_name)
@@ -192,10 +196,10 @@ def processor(abstract_url, urls_to_scrape, abstract_id_log_name, abstracts_log_
 		'''Actually obtaining the abstracts after combining ID with the abstract_url'''
 		abstract_crawler(abstract_url, abstract_id_log_name, abstracts_log_name, site_url_index, status_logger_name)
 
-def scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, status_logger_name, keywords_to_search):
+def scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, status_logger_name, keywords_to_search):
 	''''This function contains all the functions and contains this entire script here, so that it can be imported later to the main function'''
 
 	'''Provides the links for the URLs to be scraped by the scraper'''
-	urls_to_scrape = url_generator(start_url, status_logger_name)
+	urls_to_scrape = url_generator(start_url, query_string, status_logger_name)
 	'''Calling the processor() function here'''
 	processor(abstract_url, urls_to_scrape, abstract_id_log_name, abstracts_log_name, status_logger_name, keywords_to_search)
