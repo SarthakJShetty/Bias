fe7d03f4049241bbc33c6fa318772118b356db44
SarthakJShetty <sarthakshetty97@gmail.com>
2018-12-29 17:00:14 +0530

    Restructured the code, added the NLP_Engine.py, updated common_functions.py, and improved woking of Scraper.py and Visualization.py

--- Bias.py
+++ Bias.py
@@ -11,6 +11,10 @@ Sarthak J. Shetty
 from Scraper import scraper_main
 '''Importing the analyzer code here as well'''
 from Analyzer import analyzer_main
+'''Importing the visualizer and gensim code here'''
+from NLP_Engine import nlp_engine_main
+'''Importing the code to visualize the data interpreted by the NLP_Engine'''
+from Visualizer import visualizer_main
 '''Imports some of the functions required by different scripts here.'''
 from common_functions import pre_processing, arguments_parser, end_process
 '''Keywords from the user are extracted here'''
@@ -18,13 +22,19 @@ from common_functions import pre_processing, arguments_parser, end_process
 keywords_to_search=arguments_parser()
 
 '''Calling the pre_processing functions here so that data is available across the code.'''
-abstract_id_log_name, abstracts_log_name, start_url, abstract_url, status_logger_name = pre_processing(keywords_to_search)
+abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, logs_folder_name, status_logger_name = pre_processing(keywords_to_search)
 
 '''Runs the scraper here to scrape the details from the scientific repository'''
-scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, status_logger_name, keywords_to_search)
+scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, status_logger_name, keywords_to_search)
 
 '''Calling the Analyzer Function here'''
 analyzer_main(abstracts_log_name, status_logger_name)
+
+'''Calling the visualizer code below this portion'''
+lda_model, corpus, id2word = nlp_engine_main(abstracts_log_name, status_logger_name)
+
+'''Importing the visualizer_main function to view the LDA Model built by the NLP_engine_main() function'''
+visualizer_main(lda_model, corpus, id2word, logs_folder_name, status_logger_name)
 
 '''Declaring the end of a successful run of the code'''
 end_process(status_logger_name)
