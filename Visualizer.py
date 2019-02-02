789a6955b7532a83409adbdfb20bb9b329946189
SarthakJShetty <sarthakshetty97@gmail.com>
2019-02-02 14:12:40 +0530

    Sectioned and modularized the code

--- Visualizer.py
+++ Visualizer.py
@@ -10,11 +10,21 @@ Sarthak J. Shetty
 from common_functions import status_logger
 import pyLDAvis
 
-def visualizer_main(lda_model, corpus, id2word, logs_folder_name, status_logger_name):
+def visualizer_generator(lda_model, corpus, id2word, logs_folder_name, status_logger_name):
 	'''This code generates the .html file with generates the visualization of the data prepared.'''
-	visualizer_main_start_status_key = "Preparing the visualization"
-	status_logger(status_logger_name, visualizer_main_start_status_key)
+	visualizer_generator_start_status_key = "Preparing the visualization"
+	status_logger(status_logger_name, visualizer_generator_start_status_key)
 	textual_data_visualization = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
 	pyLDAvis.save_html(textual_data_visualization, logs_folder_name+"/"+"Data_Visualization.html")
-	visualizer_main_end_status_key = "Prepared the Visulizer"
+	visualizer_generator_end_status_key = "Prepared the Visulizer"
+	status_logger(status_logger_name, visualizer_generator_end_status_key)		
+
+def visualizer_main(lda_model, corpus, id2word, logs_folder_name, status_logger_name):
+	visualizer_main_start_status_key = "Entering the visualizer_main() code"
+	status_logger(status_logger_name, visualizer_main_start_status_key)
+	
+	'''This the main visualizer code. Reorging this portion of the code to ensure modularity later on as well.'''
+	visualizer_generator(lda_model, corpus, id2word, logs_folder_name, status_logger_name)
+
+	visualizer_main_end_status_key = "Exiting the visualizer_main() code"
 	status_logger(status_logger_name, visualizer_main_end_status_key)
