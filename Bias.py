db12c84cb8b8cbe5c6534b4799056a8b2d32ff24
SarthakJShetty <sarthakshetty97@gmail.com>
2019-02-09 18:37:35 +0530

    Updated with the trends histogram visualizer

--- Bias.py
+++ Bias.py
@@ -19,13 +19,13 @@ from Visualizer import visualizer_main
 from common_functions import pre_processing, arguments_parser, end_process
 '''Keywords from the user are extracted here'''
 
-keywords_to_search=arguments_parser()
+keywords_to_search, trend_keywords=arguments_parser()
 
 '''Calling the pre_processing functions here so that data is available across the code.'''
 abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, logs_folder_name, status_logger_name = pre_processing(keywords_to_search)
 
 '''Runs the scraper here to scrape the details from the scientific repository'''
-scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, status_logger_name, keywords_to_search)
+abstract_word_dictionary = scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, status_logger_name, keywords_to_search)
 
 '''Calling the Analyzer Function here'''
 analyzer_main(abstracts_log_name, status_logger_name)
@@ -34,7 +34,7 @@ analyzer_main(abstracts_log_name, status_logger_name)
 lda_model, corpus, id2word = nlp_engine_main(abstracts_log_name, status_logger_name)
 
 '''Importing the visualizer_main function to view the LDA Model built by the NLP_engine_main() function'''
-visualizer_main(lda_model, corpus, id2word, logs_folder_name, status_logger_name)
+visualizer_main(abstract_word_dictionary, trend_keywords, lda_model, corpus, id2word, logs_folder_name, status_logger_name)
 
 '''Declaring the end of a successful run of the code'''
 end_process(status_logger_name)
