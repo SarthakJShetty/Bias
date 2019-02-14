7a2c52b0b05228de7a19bac3fa6abc7fe1d5a5e2
SarthakJShetty <sarthakshetty97@gmail.com>
2019-02-14 14:16:45 +0530

    Changed the returning of variables from the common_functions.py script. Passsing the trends keyword to the scraper scripy

--- Bias.py
+++ Bias.py
@@ -25,7 +25,7 @@ keywords_to_search, trend_keywords = arguments_parser()
 abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, logs_folder_name, status_logger_name = pre_processing(keywords_to_search)
 
 '''Runs the scraper here to scrape the details from the scientific repository'''
-abstract_word_dictionary = scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, status_logger_name, keywords_to_search)
+abstract_word_dictionary, starting_year, ending_year = scraper_main(abstract_id_log_name, abstracts_log_name, start_url, abstract_url, query_string, trend_keywords, keywords_to_search, status_logger_name)
 
 '''Calling the Analyzer Function here'''
 analyzer_main(abstracts_log_name, status_logger_name)
@@ -34,7 +34,7 @@ analyzer_main(abstracts_log_name, status_logger_name)
 lda_model, corpus, id2word = nlp_engine_main(abstracts_log_name, status_logger_name)
 
 '''Importing the visualizer_main function to view the LDA Model built by the NLP_engine_main() function'''
-visualizer_main(abstract_word_dictionary, trend_keywords, lda_model, corpus, id2word, logs_folder_name, status_logger_name)
+visualizer_main(abstract_word_dictionary, starting_year, ending_year, trend_keywords, lda_model, corpus, id2word, logs_folder_name, status_logger_name)
 
 '''Declaring the end of a successful run of the code'''
 end_process(status_logger_name)
