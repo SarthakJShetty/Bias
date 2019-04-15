497a8c163fcb7051024520354d56baaba58584f6
SarthakJShetty <sarthakshetty97@gmail.com>
2019-04-15 19:17:02 +0530

    Removing this function, will reintroduce in the form of system handler scripts

--- processing_functions.py
+++
@@ -1,6 +0,0 @@
-'''Hello! This portion of the code that acts as the processing code corroborating with the main scripts [re: Scraper, Analyzer+NLP_Engine, Visualizer]
-
-- Sarthak J. Shetty
-06/02/2019
-
-from common_functions import status_logger'''
